936455acf891aad22ce46728984ca9aeb50b1710
Ivan Izdepskyi <iizdepskyy112@gmail.com>
2019-02-03 13:04:01 +0200

    Java 8 Lambda Basics 23 - Introduction To Streams

---
+++ src/com/izdebski/unit4/StreamsExample1.java
@@ -0,0 +1,24 @@
+package com.izdebski.unit4;
+
+import com.izdebski.unit1.Person;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamsExample1 {
+
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Charles", "Dickens", 60),
+                new Person("Lewis", "Carroll", 42),
+                new Person("Thomas", "Carlyle", 51),
+                new Person("Charlotte", "Bronte", 45),
+                new Person("Matthew", "Arnold", 39)
+        );
+
+        people.stream()
+                .filter(p -> p.getLastName().startsWith("C"))
+                .forEach(p -> System.out.println(p.getFirstName()));
+
+    }
+}
